8ac9df365c42054c16012c1b79db50327506858e
Ettore Di Giacinto <mudler@gentoo.org>
2020-10-16 17:08:50 +0200

    Stream output

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -15,6 +15,9 @@ package runner
 
 import (
 	"bytes"
+	"io"
+	"os"
+
 	"github.com/codeskyblue/kexec"
 	multierror "github.com/hashicorp/go-multierror"
 	"github.com/imdario/mergo"
@@ -99,12 +102,13 @@ func runProc(cmd, dir string) (string, error) {
 	p := kexec.CommandString(cmd)
 
 	var b bytes.Buffer
-	p.Stdout = &b
-	p.Stderr = &b
+	p.Stdout = io.MultiWriter(os.Stdout, &b)
+	p.Stderr = io.MultiWriter(os.Stderr, &b)
 	p.Dir = dir
 	if err := p.Run(); err != nil {
 		return b.String(), err
 	}
+
 	p.Wait()
 
 	return b.String(), nil
